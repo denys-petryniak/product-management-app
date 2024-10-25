a4d90e5c0aab6fb1224f88ffc93aeb4693bde6f1
Denys Petryniak <denys.petryniak@gmail.com>
2024-10-25 18:30:20 +0300

    feat: :sparkles: install and setup unplugin-vue-router for file-based routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,36 +1,9 @@
-import { h } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      // route level code-splitting
-      // this generates a separate chunk (Projects.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/ProjectsView.vue'),
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue'),
-    },
-    {
-      // will match everything and put it under `route.params.catchAll`
-      path: '/:catchAll(.*)*',
-      name: 'not-found',
-      component: h('p', { style: 'color: red;' }, '404 Not Found'),
-    },
-  ],
+  routes,
 })
 
 export default router

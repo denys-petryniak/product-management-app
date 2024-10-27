686de809e8c6b4f096c4401f4157ba7c4c41bbba
Denys Petryniak <denys.petryniak@gmail.com>
2024-10-27 21:35:41 +0200

    feat: :sparkles: set up supabase client in seed.js file

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,10 @@
+/* eslint-env node */
+
+import { createClient } from '@supabase/supabase-js'
 import { fakerEN_US as faker } from '@faker-js/faker'
 
-const personName = faker.person.fullName()
-const personBio = faker.person.bio()
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
-console.log(personName, personBio)
+const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
+console.log(supabase)

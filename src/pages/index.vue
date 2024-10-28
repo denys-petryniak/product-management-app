3cba04854cf2e0632ffe7bcfbb986e18ed712d14
Denys Petryniak <denys.petryniak@gmail.com>
2024-10-28 17:02:41 +0200

    feat: :sparkles: fetch projects in main page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
+const getProjects = async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) {
+    console.error(error)
+  }
+
+  console.log(data)
+}
+
+await getProjects()
+</script>
 
 <template>
   <div>

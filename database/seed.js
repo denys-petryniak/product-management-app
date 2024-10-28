76f65658a8fa1ab5d7902392b3273115b1b40942
Denys Petryniak <denys.petryniak@gmail.com>
2024-10-28 15:13:27 +0200

    feat: :sparkles: seed projects table

--- database/seed.js
+++ database/seed.js
@@ -1,5 +1,4 @@
 /* eslint-env node */
-
 import { createClient } from '@supabase/supabase-js'
 import { fakerEN_US as faker } from '@faker-js/faker'
 
@@ -7,4 +6,20 @@ const supabaseUrl = process.env.VITE_SUPABASE_URL
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
 
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
-console.log(supabase)
+
+const seedProjects = async () => {
+  const name = faker.lorem.words(3)
+
+  const { error } = await supabase.from('projects').insert({
+    name,
+    slug: faker.helpers.slugify(name),
+    status: faker.helpers.arrayElement(['in-progress', 'completed']),
+    collaborators: faker.helpers.arrayElements([1, 2, 3]),
+  })
+
+  if (error) {
+    console.error(error)
+  }
+}
+
+await seedProjects()

95c838895d1c63746d578a4ccd4a7f15abae2dc9
Denys Petryniak <denys.petryniak@gmail.com>
2024-10-29 21:14:33 +0200

    feat: :sparkles: install & setup shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,17 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),

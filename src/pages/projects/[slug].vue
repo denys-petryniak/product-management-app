e072f29683c0251ffdccbb4b20085a4a78825f0a
Denys Petryniak <denys.petryniak@gmail.com>
2024-11-03 18:28:08 +0200

    feat(project): :sparkles: add static template and fetch data

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -1,10 +1,122 @@
 <script setup lang="ts">
+import { projectQuery } from '@/utils/supabaseQueries'
+import type { Project } from '@/utils/supabaseQueries'
+
+const project = ref<Project | null>(null)
 const route = useRoute('/projects/[slug]')
+
+const getProject = async () => {
+  const { data, error } = await projectQuery(route.params.slug)
+
+  if (error) {
+    console.error(error)
+  }
+
+  project.value = data
+}
+
+await getProject()
 </script>
 
 <template>
-  <div>
-    <h1>Project {{ route.params?.slug }}</h1>
-    <RouterLink to="/">Go to Home</RouterLink>
-  </div>
+  <Table>
+    <TableRow>
+      <TableHead> Name </TableHead>
+      <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Description </TableHead>
+      <TableCell>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure qui
+        tempora ex nihil, ab reprehenderit dolorem sunt veritatis perferendis?
+        Repudiandae quis velit quasi ab natus quia ratione voluptas deserunt
+        labore sed distinctio nam fuga fugit vero voluptates placeat aperiam,
+        saepe excepturi eos harum consectetur doloremque perspiciatis nesciunt!
+        Incidunt, modi.
+      </TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Status </TableHead>
+      <TableCell>In progress</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead> Collaborators </TableHead>
+      <TableCell>
+        <div class="flex">
+          <Avatar
+            class="-mr-4 border border-primary transition-transform hover:scale-110"
+            v-for="n in 5"
+            :key="n"
+          >
+            <RouterLink
+              class="flex h-full w-full items-center justify-center"
+              to=""
+            >
+              <AvatarImage src="" alt="" />
+              <AvatarFallback> </AvatarFallback>
+            </RouterLink>
+          </Avatar>
+        </div>
+      </TableCell>
+    </TableRow>
+  </Table>
+  <section class="mt-10 flex grow flex-col justify-between gap-5 md:flex-row">
+    <div class="flex-1">
+      <h2>Tasks</h2>
+      <div class="table-container">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead> Name </TableHead>
+              <TableHead> Status </TableHead>
+              <TableHead> Due Date </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow v-for="n in 5" :key="n">
+              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+              <TableCell> In progress </TableCell>
+              <TableCell> 22/08/2024 </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+    <div class="flex-1">
+      <h2>Documents</h2>
+      <div class="table-container">
+        <p class="px-4 py-3 text-sm font-semibold text-muted-foreground">
+          This project doesn't have documents yet...
+        </p>
+        <!-- <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead> Name </TableHead>
+              <TableHead> Visibility </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+              <TableCell> Private </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table> -->
+      </div>
+    </div>
+  </section>
 </template>
+
+<style>
+th {
+  @apply w-[100px];
+}
+
+h2 {
+  @apply mb-4 w-fit text-lg font-semibold;
+}
+
+.table-container {
+  @apply h-80 overflow-hidden overflow-y-auto rounded-md bg-slate-900;
+}
+</style>
--- src/utils/supabaseQueries.ts
+++ src/utils/supabaseQueries.ts
@@ -1,6 +1,7 @@
 import { supabase } from '@/lib/supabaseClient'
 import type { QueryData } from '@supabase/supabase-js'
 
+// FIXME: inferred types are not working
 export const tasksWithProjectsQuery = supabase.from('tasks').select(`
   *,
   projects (
@@ -9,7 +10,28 @@ export const tasksWithProjectsQuery = supabase.from('tasks').select(`
     slug
   )
 `)
+
 export type TaskWithProjects = QueryData<typeof tasksWithProjectsQuery>
 
 export const projectsQuery = supabase.from('projects').select()
+
 export type Projects = QueryData<typeof projectsQuery>
+
+export const projectQuery = (slug: string) =>
+  supabase
+    .from('projects')
+    .select(
+      `
+      *,
+      tasks (
+        id,
+        name,
+        status,
+        due_date,
+      )
+    `,
+    )
+    .eq('slug', slug)
+    .single()
+
+export type Project = QueryData<ReturnType<typeof projectQuery>>

090c9ef01b3f2bc3f3d1d5bd745490fecc11237f
Denys Petryniak <denys.petryniak@gmail.com>
2024-11-15 11:33:24 +0200

    feat(auth): :sparkles: use v-model to collect form data values on register form

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,4 +1,13 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const formData = ref({
+  userName: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+})
+</script>
 
 <template>
   <div
@@ -19,16 +28,34 @@
         <form class="grid gap-4">
           <div class="grid gap-2">
             <Label id="username" class="text-left">Username</Label>
-            <Input id="username" type="text" placeholder="johndoe19" required />
+            <Input
+              id="username"
+              type="text"
+              placeholder="johndoe19"
+              required
+              v-model="formData.userName"
+            />
           </div>
           <div class="flex flex-col justify-between gap-4 sm:flex-row">
             <div class="grid gap-2">
               <Label id="first_name" class="text-left">First Name</Label>
-              <Input id="first_name" type="text" placeholder="John" required />
+              <Input
+                id="first_name"
+                type="text"
+                placeholder="John"
+                required
+                v-model="formData.firstName"
+              />
             </div>
             <div class="grid gap-2">
               <Label id="last_name" class="text-left">Last Name</Label>
-              <Input id="last_name" type="text" placeholder="Doe" required />
+              <Input
+                id="last_name"
+                type="text"
+                placeholder="Doe"
+                required
+                v-model="formData.lastName"
+              />
             </div>
           </div>
           <div class="grid gap-2">
@@ -38,6 +65,7 @@
               type="email"
               placeholder="johndoe19@example.com"
               required
+              v-model="formData.email"
             />
           </div>
           <div class="grid gap-2">
@@ -48,6 +76,7 @@
               placeholder="*****"
               autocomplete
               required
+              v-model="formData.password"
             />
           </div>
           <div class="grid gap-2">
@@ -60,9 +89,10 @@
               placeholder="*****"
               autocomplete
               required
+              v-model="formData.confirmPassword"
             />
           </div>
-          <Button type="submit" class="w-full"> Register </Button>
+          <Button type="submit" class="w-full">Register</Button>
           <!-- <Button variant="outline" class="w-full"> Login with Google </Button> -->
         </form>
         <div class="mt-4 text-center text-sm">

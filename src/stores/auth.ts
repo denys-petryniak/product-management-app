0b7029fb1d20a3e1bb72e5e36001076db919d8e2
Denys Petryniak <denys.petryniak@gmail.com>
2024-11-16 18:08:58 +0200

    feat(auth): :sparkles: set up auth store & integrate it with utility functions

---
+++ src/stores/auth.ts
@@ -0,0 +1,23 @@
+import type { Session, User } from '@supabase/supabase-js'
+import type { Tables } from 'database/types'
+
+export const useAuthStore = defineStore('auth-store', () => {
+  const user = ref<User | null>(null)
+  const profile = ref<Tables<'profiles'> | null>(null)
+
+  const setAuth = (userSession: Session | null = null) => {
+    if (!userSession) {
+      user.value = null
+
+      return
+    }
+
+    user.value = userSession.user
+  }
+
+  return {
+    user,
+    profile,
+    setAuth,
+  }
+})

c1083e59aa594db472662f6faf3ed1fcad903232
Denys Petryniak <denys.petryniak@gmail.com>
2024-12-22 20:31:49 +0200

    feat(projects): :sparkles: add AppInPlaceEditText component for inline editing of project name

---
+++ src/components/AppInPlaceEdit/AppInPlaceEditText.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const value = defineModel()
+</script>
+
+<template>
+  <input
+    type="text"
+    v-model="value"
+    class="w-full bg-transparent p-1 focus:rounded-md focus:border-none focus:bg-gray-800 focus:outline-none"
+  />
+</template>

309272433dc9e8e874238c3332b55410429d680e
Denys Petryniak <denys.petryniak@gmail.com>
2024-12-22 20:49:03 +0200

    feat(projects): :sparkles: add commit event to AppInPlaceEditText for project name updates

--- src/components/AppInPlaceEdit/AppInPlaceEditText.vue
+++ src/components/AppInPlaceEdit/AppInPlaceEditText.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 const value = defineModel()
+
+defineEmits(['commit'])
 </script>
 
 <template>
   <input
     type="text"
     v-model="value"
+    @blur="$emit('commit')"
+    @keyup.enter="($event.target as HTMLInputElement).blur()"
     class="w-full bg-transparent p-1 focus:rounded-md focus:border-none focus:bg-gray-800 focus:outline-none"
   />
 </template>
--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -21,7 +21,10 @@ await getProject(slug)
     <TableRow>
       <TableHead>Name</TableHead>
       <TableCell>
-        <AppInPlaceEditText v-model="project.name" />
+        <AppInPlaceEditText
+          v-model="project.name"
+          @commit="console.log('Changed')"
+        />
       </TableCell>
     </TableRow>
     <TableRow>

61d54dd05d1b923824f54f83ba0f45c2f145a84c
Denys Petryniak <denys.petryniak@gmail.com>
2025-01-16 11:31:13 +0200

    feat(tasks): :sparkles: implement task creation form

--- src/components/AppNew/AppNewTask.vue
+++ src/components/AppNew/AppNewTask.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import { FormKit } from '@formkit/vue'
+import type { FormData } from '@/types/TaskFormData'
 
 const isSheetOpen = defineModel<boolean>()
+
+const createTask = async (formData: FormData) => {
+  await new Promise(resolve => {
+    setTimeout(() => {
+      resolve(console.log('Task created', formData))
+    }, 2000)
+  })
+}
 </script>
 
 <template>
@@ -10,7 +19,44 @@ const isSheetOpen = defineModel<boolean>()
       <SheetHeader>
         <SheetTitle>Create new Task</SheetTitle>
       </SheetHeader>
-      <FormKit type="text" />
+      <FormKit type="form" @submit="createTask" submit-label="Create Task">
+        <FormKit
+          type="text"
+          name="name"
+          id="name"
+          label="Name"
+          placeholder="My new task"
+        />
+        <FormKit
+          type="select"
+          name="for"
+          id="for"
+          label="For"
+          placeholder="Select a user"
+          :options="[
+            { label: 'User 1', value: 1 },
+            { label: 'User 2', value: 2 },
+          ]"
+        />
+        <FormKit
+          type="textarea"
+          name="description"
+          id="description"
+          label="Description"
+          placeholder="Task description"
+        />
+        <FormKit
+          type="select"
+          name="project"
+          id="project"
+          label="Project"
+          placeholder="Select a project"
+          :options="[
+            { label: 'Project 1', value: 1 },
+            { label: 'Project 2', value: 2 },
+          ]"
+        />
+      </FormKit>
     </SheetContent>
   </Sheet>
 </template>
---
+++ src/types/TaskFormData.ts
@@ -0,0 +1,6 @@
+export interface FormData {
+  name: string
+  description: string
+  profile_id: string
+  project: number
+}
